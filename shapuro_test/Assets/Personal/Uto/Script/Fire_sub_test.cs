22e8bc4d9b9b754b5debd84541d6ccbcb9f2d04a
UtoMasato <hw22a024@oecu.jp>
2024-06-05 21:23:24 +0900

    Add: テストシーンで色々試しに作ってみた

---
+++ shapuro_test/Assets/Personal/Uto/Script/Fire_sub_test.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_sub_test : MonoBehaviour
+{
+    [SerializeField] private Fire_test parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (parent.GetIsOnCandle() && other.gameObject.CompareTag("lamp"))
+        {
+            other.gameObject.GetComponent<Lamp_test>().Ignition();
+        }
+
+        if (!parent.GetIsOnCandle() && other.gameObject.CompareTag("FirePosition"))
+        {
+            parent.Transfer(other.gameObject.GetComponent<FirePosition_test>().candle);
+        }
+    }
+}
