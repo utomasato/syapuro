3bc3a6fa2b0bbed3f6b05ea062345c9a4d04c7e8
Itsuki Imanaka <hw22a019@oecu.jp>
2024-06-11 13:16:42 +0900

    ゲームオーバー時のスクリプトを追加しました

---
+++ shapuro_test/Assets/Personal/Imanaka/ScreenColor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScreenColor : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject GameOvercanvas;
+    private GameManager gamemanager;//SetGameクラスのGameManagerクラスを使用
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamemanager = new GameManager();
+        GameOvercanvas.SetActive(false);
+        //   gamemanager.SetisGameOver(true); /*ここをコメント解除するとゲームオーバー画面になる*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gamemanager.GetisGameOver())
+        {
+            GameOvercanvas.SetActive(true);
+        }
+    }
+}
