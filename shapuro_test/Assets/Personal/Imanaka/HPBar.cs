77aa237f5a5b39ee5eb84819a3ffdc9416178c5c
Itsuki Imanaka <hw22a019@oecu.jp>
2024-07-07 10:44:45 +0900

    HPバーのコードを少しだけ追加

---
+++ shapuro_test/Assets/Personal/Imanaka/HPBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HPBar : MonoBehaviour
+{
+    [SerializeField]
+    private Candle candle;
+
+    [SerializeField]
+    public Slider hpBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- shapuro_test/Assets/Personal/Uto/Script/Candle.cs
+++ shapuro_test/Assets/Personal/Uto/Script/Candle.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Candle : MonoBehaviour
 {
     [SerializeField] private float startLife; // 初期ライフ
@@ -14,10 +14,14 @@ public class Candle : MonoBehaviour
     [SerializeField] private GameObject foot; // ロウソクの足
     [SerializeField] private float footSize; // 足が生えたときの高さの増加分
 
+    /*急用でほとんどできてないのでバーのHPバーのコードは無視してもらって大丈夫です*/
+    [SerializeField] private Slider HPbar;//ロウソクの体力バー
+
     private bool IsBurning = false; // ロウソクが燃えているかどうか
     private bool CanJump; // ジャンプ可能かどうか
     private bool IsBurnOut = false; // ロウソクが燃え尽きたかどうか
     private Rigidbody rb; // 物理挙動のためのRigidbodyコンポーネント
+
 
     void Start()
     {
@@ -63,6 +67,13 @@ public class Candle : MonoBehaviour
         body.transform.localScale = ls;
         if (IsBurning) ls.y += footSize; // 燃えている場合はサイズを調整
         transform.localScale = ls;
+        if (HPbar != null)
+        {
+            HPbar.value = (float)size / startSize;
+            Debug.Log(size);
+
+
+        }
     }
 
     public void Move(float x) // ロウソクを水平に移動
@@ -114,6 +125,10 @@ public class Candle : MonoBehaviour
         Debug.Log(this.name + " : BurnOut"); // 燃え尽きたことをログに出力
         IsBurnOut = true;
         transform.parent.gameObject.SetActive(false); // 親オブジェクトを非アクティブ化
+        if (HPbar != null)
+        {
+            HPbar.value = 0;
+        }
     }
 
     public float GetSize() // 現在のサイズを返す
