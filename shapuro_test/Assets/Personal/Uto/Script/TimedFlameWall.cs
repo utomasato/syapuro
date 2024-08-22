59531fc509b8277dac13994a8547c5b260be3266
UtoMasato <hw22a024@oecu.jp>
2024-08-22 11:12:40 +0900

    Update: 時限式の炎の壁に円ゲージをつけた

--- shapuro_test/Assets/Personal/Uto/Script/TimedFlameWall.cs
+++ shapuro_test/Assets/Personal/Uto/Script/TimedFlameWall.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimedFlameWall : MonoBehaviour
 {
@@ -9,12 +10,24 @@ public class TimedFlameWall : MonoBehaviour
     [SerializeField] private float initialTime;
     private Vector3 size;
     private GameState gameState;
+    [SerializeField] Image BurningGauge, SilentGauge;
 
     void Start()
     {
         elapsedTime = 0f;
         size = new Vector3(1f, 1f, 1f);
         gameState = GameObject.Find("GameState").GetComponent<GameState>();
+
+        if (size.y > 0f)
+        {
+            BurningGauge.fillAmount = 1f - (elapsedTime + initialTime) / interval % 2f;
+            SilentGauge.fillAmount = 0f;
+        }
+        else
+        {
+            BurningGauge.fillAmount = 1f;
+            SilentGauge.fillAmount = 2f - (elapsedTime + initialTime) / interval % 2f;
+        }
 
         size.y = Mathf.PingPong((elapsedTime + initialTime) * 2 / interval + 1f, 2f) - 1f;
         size.y = Mathf.Clamp(size.y * interval * 8f, 0f, 1f);
@@ -26,6 +39,18 @@ public class TimedFlameWall : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             size.y = Mathf.PingPong((elapsedTime + initialTime) * 2 / interval + 1f, 2f) - 1f;
+            if (size.y > 0f)
+            {
+                //Debug.Log(1f - (elapsedTime + initialTime) / interval % 2f);
+                BurningGauge.fillAmount = 1f - (elapsedTime + initialTime) / interval % 2f;
+                SilentGauge.fillAmount = 0f;
+            }
+            else
+            {
+                BurningGauge.fillAmount = 1f;
+                //Debug.Log(2f - (elapsedTime + initialTime) / interval % 2f);
+                SilentGauge.fillAmount = 2f - (elapsedTime + initialTime) / interval % 2f;
+            }
             size.y = Mathf.Clamp(size.y * interval * 8f, 0f, 1f);
             transform.localScale = size;
         }
