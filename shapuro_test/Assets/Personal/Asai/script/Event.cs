6e3da9dbf20a16e1864e4630b054eec16465b175
UtoMasato <hw22a024@oecu.jp>
2024-08-22 16:28:16 +0900

    Update: アルファー版前の調整をした

--- shapuro_test/Assets/Personal/Asai/script/Event.cs
+++ shapuro_test/Assets/Personal/Asai/script/Event.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Event : MonoBehaviour
@@ -9,6 +10,14 @@ public class Event : MonoBehaviour
     public Fire fire_script;
     public GameState GameState;
     public GameObject Event_img;
+    public UnityEngine.UI.Button button;
+    public enum Mode
+    {
+        Mode_Next01,
+        Mode_Next02,
+        Mode_End
+    }
+    public Mode mode;
     void Start()
     {
         GameObject Gamestate = GameObject.Find("Player");
@@ -16,15 +25,12 @@ public class Event : MonoBehaviour
     }
     void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.name == "Fire" && used == false)
+        if (other.gameObject.name == "Fire" && used == false && GameState.JudgeState("GamePlay"))
         {
             Event_img.SetActive(true);
             GameState.OpenExplain();
             used = true;
-        }
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
-        {
-            StartCoroutine(ExecuteAfterOneFrame());
+            EventSystem.current.SetSelectedGameObject(button.gameObject);
         }
     }
     // void OnTriggerExit(Collider other)
@@ -34,13 +40,34 @@ public class Event : MonoBehaviour
     //         Event_img.SetActive(false);
     //     }
     // }
-    public void next()
+    public void next01()
     {
-        Debug.Log("aaa");
+        Debug.Log("next01");
         if (Event_img.gameObject.name == "Event01_img")
         {
             Event_img.SetActive(false);
-            GameState.CloseExplain();
+            //Debug.Log("aaa");
+        }
+    }
+    public void next02()
+    {
+        Debug.Log("next02");
+        if (Event_img.gameObject.name == "Event01_img")
+        {
+            Event_img.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(button.gameObject);
+            //Debug.Log("aaa");
+        }
+    }
+    public void end()
+    {
+        Debug.Log("end");
+        if (Event_img.gameObject.name == "Event01_img")
+        {
+            StartCoroutine(ExecuteAfterOneFrame());
+            //Event_img.SetActive(false);
+            //GameState.CloseExplain();
+
             //Debug.Log("aaa");
         }
     }
@@ -48,7 +75,22 @@ public class Event : MonoBehaviour
     IEnumerator ExecuteAfterOneFrame() // 実行を１フレーム遅らせる
     {
         yield return null; // 1フレーム待つ
-        next();
+        GameState.CloseExplain();
+        Event_img.SetActive(false);
+        /*
+        if (mode == Mode.Mode_Next01)
+        {
+            next01();
+        }
+        if (mode == Mode.Mode_Next02)
+        {
+            next02();
+        }
+        if (mode == Mode.Mode_End)
+        {
+            end();
+        }
+        */
     }
 
 }
