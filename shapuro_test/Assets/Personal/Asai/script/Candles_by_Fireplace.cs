7d2cf1003ba6e4de699be8f2f96f90fbbd0555f3
UtoMasato <hw22a024@oecu.jp>
2024-09-21 15:40:44 +0900

    Update: 定例中作業

--- shapuro_test/Assets/Personal/Asai/script/Candles_by_Fireplace.cs
+++ shapuro_test/Assets/Personal/Asai/script/Candles_by_Fireplace.cs
@@ -29,26 +29,42 @@ public class Candles_by_Fireplace : MonoBehaviour
     }
     public void create()
     {
-        var oneCycle = 2.0f * Mathf.PI; // sin の周期は 2π
-
-        for (var i = 0; i < MaxLampCount; ++i)
+        if (MaxLampCount == 1)
         {
-
-            var point = ((float)i / (MaxLampCount - 1.0f)) * oneCycle; // 周期の位置 (1.0 = 100% の時 2π となる)
-            var repeatPoint = point * repeat; // 繰り返し位置
-
-            var x = parent.transform.position.x + Mathf.Cos(repeatPoint) * radius;
-            var y = parent.transform.position.y + Mathf.Sin(repeatPoint) * radius - 0.5f;
-
+            var x = parent.transform.position.x;
+            var y = parent.transform.position.y + 1.5f;
             var position = new Vector3(x, y);
 
             GameObject LampInstance = Instantiate(
-                createObject,
-                position,
-                Quaternion.identity,
-                transform
-            );
+                    createObject,
+                    position,
+                    Quaternion.identity,
+                    transform
+                );
             dinplayLampList.Add(LampInstance);
+        }
+        else
+        {
+            var oneCycle = 2.0f * Mathf.PI; // sin の周期は 2π
+            for (var i = 0; i < MaxLampCount; ++i)
+            {
+
+                var point = ((float)i / (MaxLampCount - 1.0f)) * oneCycle; // 周期の位置 (1.0 = 100% の時 2π となる)
+                var repeatPoint = point * repeat; // 繰り返し位置
+
+                var x = parent.transform.position.x + Mathf.Cos(repeatPoint) * radius;
+                var y = parent.transform.position.y + Mathf.Sin(repeatPoint) * radius - 0.5f;
+
+                var position = new Vector3(x, y);
+
+                GameObject LampInstance = Instantiate(
+                    createObject,
+                    position,
+                    Quaternion.identity,
+                    transform
+                );
+                dinplayLampList.Add(LampInstance);
+            }
         }
     }
 }
