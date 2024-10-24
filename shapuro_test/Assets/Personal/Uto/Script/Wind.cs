ca4e4516b51950e0848a2b9c5f334f81e555b1b2
UtoMasato <hw22a024@oecu.jp>
2024-10-24 18:34:32 +0900

    Add: 風にパーティクルをつけた

---
+++ shapuro_test/Assets/Personal/Uto/Script/Wind.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] GameObject windArea;
+    [SerializeField] ParticleSystem windParticle;
+    [SerializeField] float range;
+    [SerializeField] float particleMargin;
+    ParticleSystem.MainModule main;
+    Vector3 scale;
+
+    void Start()
+    {
+        main = windParticle.main;
+        scale = windArea.transform.localScale;
+        scale.y = range;
+        windArea.transform.localScale = scale;
+        main.startLifetime = (range + particleMargin) / 100;
+    }
+
+    void Update()
+    {
+        ChangeRange(range);
+    }
+
+    public void ChangeRange(float r)
+    {
+        range = r;
+        scale.y = range;
+        windArea.transform.localScale = scale;
+        main.startLifetime = (range + particleMargin) / 100;
+    }
+
+}
